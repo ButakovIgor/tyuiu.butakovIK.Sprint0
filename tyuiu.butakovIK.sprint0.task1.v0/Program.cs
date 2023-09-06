a96951f2d2c949dd14aa33224a4d243352b49cef
ButakovIgor <t4w3rr@gmail.com>
2023-09-06 18:27:41 +0500

    task1

---
+++ tyuiu.butakovIK.sprint0.task1.v0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.butakovIK.sprint0.task1.v0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, Мир");
+            string name = "Игорь";
+            int age = 22;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + ", мне " + age + " года");
+            Console.ReadKey();       
+        }
+    }
+}
