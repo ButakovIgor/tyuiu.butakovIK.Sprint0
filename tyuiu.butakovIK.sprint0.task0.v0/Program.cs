6f9dcb6916624b0ff0b506b451677b9e84f19c37
t4w3r <t4w3r@PC>
2023-09-06 17:59:37 +0500

    Add project files.

---
+++ tyuiu.butakovIK.sprint0.task0.v0/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.butakovIK.sprint0.task0.v0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
