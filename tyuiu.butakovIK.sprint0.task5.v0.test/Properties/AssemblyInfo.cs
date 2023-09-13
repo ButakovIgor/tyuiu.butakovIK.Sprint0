aee4e5974057af0452492ee96a45d075028de7dd
ButakovIgor <t4w3rr@gmail.com>
2023-09-13 22:20:48 +0500

    Task5

---
+++ tyuiu.butakovIK.sprint0.task5.v0.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("tyuiu.butakovIK.sprint0.task5.v0.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tyuiu.butakovIK.sprint0.task5.v0.test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("21e47141-d039-4192-a6dc-7bf0858a9906")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ tyuiu.butakovIK.sprint0.task5.v0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.butakovIK.sprint0.task5.v0.lib;
+
+namespace tyuiu.butakovIK.sprint0.task5.v0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B =" + DataService.Addition(10, 5));
+            Console.WriteLine("A - B =" + DataService.Subtraction(20, 5));
+            Console.WriteLine("A * B =" + DataService.Multiplication(10, 15));
+            Console.WriteLine("A / B =" + DataService.Division(10, 5));
+            Console.ReadKey();
+        }
+    }
+}
