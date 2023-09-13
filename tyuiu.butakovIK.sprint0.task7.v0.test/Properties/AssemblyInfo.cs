ad1f8483bf4b58a04756c98726751deda0fd486c
ButakovIgor <t4w3rr@gmail.com>
2023-09-14 00:03:40 +0500

    task7

---
+++ tyuiu.butakovIK.sprint0.task7.v0.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("tyuiu.butakovIK.sprint0.task7.v0.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tyuiu.butakovIK.sprint0.task7.v0.test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("88a98f98-764c-4f18-af42-e60e1d90b6ec")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
